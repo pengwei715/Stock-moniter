efc2a35ddbcad6d22b0a780103caaf0190041d38
pgw <pengwei715@gmail.com>
2020-02-27 18:35:50 -0600

    formatting

--- hw4/src/components/datepick.jsx
+++ hw4/src/components/datepick.jsx
@@ -1,57 +1,58 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
 class DatePanel extends React.Component {
-
   state = {
     startDate: new Date()
-  }
+  };
   handleChange = date => {
-    this.setState({startDate:date},
-     this.sendDate(date))
-  }
+    this.setState({ startDate: date }, this.sendDate(date));
+  };
 
-  sendDate = (date) => {
-    this.props.parentCallback(date)
-  }
-
+  sendDate = date => {
+    this.props.parentCallback(date);
+  };
 
   render() {
-      return (
-<div className='row'>
-        <div className=" col-md-2" style={{backgroud:'black'}} >
-            <div className="card-img align-items-center text-center">
+    return (
+      <div className="row">
+        <div className=" col-md-2" style={{ backgroud: "black" }}>
+          <div className="card-img align-items-center text-center">
             <DatePicker
-            selected={this.state.startDate}
-            onChange={this.handleChange}
-            onSelect={(date )=>{ this.setState({startDate:date},this.sendDate(date))}}
-            inline
-            fixedHeight
+              selected={this.state.startDate}
+              onChange={this.handleChange}
+              onSelect={date => {
+                this.setState({ startDate: date }, this.sendDate(date));
+              }}
+              inline
+              fixedHeight
             />
+          </div>
+        </div>
+
+        <h3 className="col-md-3 text-light ">
+          There are so many hazardous asteroids passing by the earth, pick a
+          date, we will show you the the rank of the Hazardous Asteroids
+        </h3>
+
+        <div className=" mb-3 col-md-6">
+          <div className="card-img align-items-center text-center">
+            <iframe
+              width="800"
+              height="205"
+              src="https://www.youtube.com/embed/FiNU1BwNH5I"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              autoPlay
+            ></iframe>
+          </div>
         </div>
       </div>
-
-      <h3 className='col-md-3 text-light '>
-          There are so many hazardous asteroids 
-          passing by the earth, pick a date, we will show you 
-          the the rank of the Hazardous Asteroids 
-      </h3>
-
-
-      <div className=" mb-3 col-md-6">
-        <div className="card-img align-items-center text-center">
-        <iframe width="800" height="205" src="https://www.youtube.com/embed/FiNU1BwNH5I" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen autoPlay>
-
-        </iframe>
-        </div>
-      </div>
-    </div>
-    )
+    );
   }
 }
 
-
-export default DatePanel
+export default DatePanel;
--- hw4/src/components/discribes.jsx
+++ hw4/src/components/discribes.jsx
@@ -1,109 +1,125 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-import { urlForAllNeo, getLastweek} from '@/api'
-
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import { urlForAllNeo, getLastweek } from "@/api";
 
 class Discription extends React.Component {
-  _mounted = false
-  constructor(){
-    super()
+  _mounted = false;
+  constructor() {
+    super();
     this.state = {
       date: new Date(),
       begin: 0,
       end: 0,
-      numTotal:0,
+      numTotal: 0,
       numDanger: 0,
       numOversize: 0,
       dangers: []
-    }  
+    };
   }
-  componentDidUpdate(prevProps){
-   
-    console.log("in update")
-    if (prevProps.time !== this.props.time ) {
-        this.setState(
-          {date: this.props.time,
-        }, this.componentDidMount())  
-        
-      }   
+  componentDidUpdate(prevProps) {
+    console.log("in update");
+    if (prevProps.time !== this.props.time) {
+      this.setState({ date: this.props.time }, this.componentDidMount());
+    }
   }
 
-    sendData = () => {
-      this.props.callbackFromParent(this.state.dangers)
-    }
-  
+  sendData = () => {
+    this.props.callbackFromParent(this.state.dangers);
+  };
+
   componentDidMount() {
-    this._mounted = true
-    var dates = getLastweek(this.props.time)
-    var today = new Date(this.props.time.getTime())
-    var st = today.toISOString().substring(0, 10)
-    this.setState({begin:st})
-    today.setDate(today.getDate() - 6)
-    var ed = today.toISOString().substring(0, 10)
-    this.setState({end:ed})
-    var url = urlForAllNeo(st, ed)
-    fetch(url).then((res) => {
-        return res.json()
-    }).then((data) => {
-        if (this._mounted) {
-            this.setState({numTotal:data.element_count})
-        }
-        return data.near_earth_objects
-    }).then((neos) => {
-      var neoLst = []
-      var num1 = 0
-      var num2 = 0
-      dates.forEach(date=> {
-        if (neos[date]){
-        for (const item of neos[date]){
-          if(item.is_potentially_hazardous_asteroid){
-            this.setState({numDanger: ++num1})
-            neoLst.push(item)
-            this.setState({dangers: neoLst})
-          if(item.estimated_diameter.meters.estimated_diameter_max > 100){
-            this.setState({numOversize: ++num2})
-          }
-          }
-        }
-      }   
-        })
+    this._mounted = true;
+    var dates = getLastweek(this.props.time);
+    var today = new Date(this.props.time.getTime());
+    var st = today.toISOString().substring(0, 10);
+    this.setState({ begin: st });
+    today.setDate(today.getDate() - 6);
+    var ed = today.toISOString().substring(0, 10);
+    this.setState({ end: ed });
+    var url = urlForAllNeo(st, ed);
+    fetch(url)
+      .then(res => {
+        return res.json();
       })
-      console.log(this.state.dangers)
-    }
+      .then(data => {
+        if (this._mounted) {
+          this.setState({ numTotal: data.element_count });
+        }
+        return data.near_earth_objects;
+      })
+      .then(neos => {
+        var neoLst = [];
+        var num1 = 0;
+        var num2 = 0;
+        dates.forEach(date => {
+          if (neos[date]) {
+            for (const item of neos[date]) {
+              if (item.is_potentially_hazardous_asteroid) {
+                this.setState({ numDanger: ++num1 });
+                neoLst.push(item);
+                this.setState({ dangers: neoLst });
+                if (
+                  item.estimated_diameter.meters.estimated_diameter_max > 100
+                ) {
+                  this.setState({ numOversize: ++num2 });
+                }
+              }
+            }
+          }
+        });
+      });
+    console.log(this.state.dangers);
+  }
 
-  componentWillUnmount(){
-    this._mounted = false
+  componentWillUnmount() {
+    this._mounted = false;
   }
   render() {
     return (
-    <div className="card-deck" onMouseMove = {()=>this.sendData()}>
+      <div className="card-deck" onMouseMove={() => this.sendData()}>
         <div className="card text-white bg-dark mb-3">
-            <h3 className="card-header text-center" >Total Number of Asteroids of the week</h3>
-            <div className="card-body">
-            <h1 className="card-text text-center text-danger">{this.state.numTotal}</h1>
-            </div>
-    <div className="card-footer text-center">Total number from {this.state.end} to {this.state.begin}</div>
-      </div>
-      <div className="card text-white bg-dark mb-3" >
-        <h3 className="card-header text-center">Number of Hazardous Asteroids</h3>
-        <div className="card-body">
-        <h1 className="card-text text-center text-danger">{this.state.numDanger}</h1>
+          <h3 className="card-header text-center">
+            Total Number of Asteroids of the week
+          </h3>
+          <div className="card-body">
+            <h1 className="card-text text-center text-danger">
+              {this.state.numTotal}
+            </h1>
+          </div>
+          <div className="card-footer text-center">
+            Total number from {this.state.end} to {this.state.begin}
+          </div>
         </div>
-        <div className="card-footer text-center">NASA label Hazardous from {this.state.end} to {this.state.begin}</div>
-      </div>
-      <div className="card text-white bg-dark mb-3" >
-        <h3 className="card-header text-center">Number of Over-sized Asteroids</h3>
-        <div className="card-body">
-        <h1 className="card-text text-center text-danger">{this.state.numOversize}</h1>
-
+        <div className="card text-white bg-dark mb-3">
+          <h3 className="card-header text-center">
+            Number of Hazardous Asteroids
+          </h3>
+          <div className="card-body">
+            <h1 className="card-text text-center text-danger">
+              {this.state.numDanger}
+            </h1>
+          </div>
+          <div className="card-footer text-center">
+            NASA label Hazardous from {this.state.end} to {this.state.begin}
+          </div>
         </div>
-        <div className="card-footer text-center">diamter > 100 meters from {this.state.end} to {this.state.begin}</div>
+        <div className="card text-white bg-dark mb-3">
+          <h3 className="card-header text-center">
+            Number of Over-sized Asteroids
+          </h3>
+          <div className="card-body">
+            <h1 className="card-text text-center text-danger">
+              {this.state.numOversize}
+            </h1>
+          </div>
+          <div className="card-footer text-center">
+            diamter > 100 meters from {this.state.end} to {this.state.begin}
+          </div>
+        </div>
       </div>
-      </div>
-    
     );
   }
-};
+}
 
-export default Discription
+export default Discription;
